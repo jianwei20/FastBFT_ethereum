c41ff942feb1def265d40739b3e11cea7637cd1e
jianwei20 <102753053@nccu.edu.tw>
2019-03-27 02:44:40 +0800

    aws_start1

--- experiment/aws_start1.sh
+++ experiment/aws_start1.sh
@@ -3,18 +3,23 @@
 # $2 size of block
 # $3 number of the node in the ip
 
-array=(10.0.1.70 10.0.1.65)
+array=(10.0.1.227 10.0.1.252)
 user=”ubuntu″
-echo "------------pull Address from enode--------------"
+
+echo "------In start.sh, start ${1} nodes------"
+
 x=1
 for i in "${array[@]}";
 do
 y=1
 while (($y<=$3))
 do
-        echo " cope $x"
-        ssh -i ssh-fsbft.pem ubuntu@$i nohup ./home/ubuntu/FastBFT_ethereum/experiment/node$x.sh &> test.log
+        echo "start node $x in node$y"
+        ssh -i ssh-fsbft.pem ubuntu@$i "cd /home/ubuntu/FastBFT_ethereum/experiment; nohup sh /home/ubuntu/FastBFT_ethereum/experiment/node${x}.sh &> /home/ubuntu/FastBFT_ethereum/experiment/data1/n${x}.log &"
+         ssh -i ssh-fsbft.pem ubuntu@$i "pwd"
+        #ssh -i ssh-fsbft.pem ubuntu@$i "cd /home/ubuntu/FastBFT_ethereum/experiment; ../build/bin/geth --exec 'admin.nodeInfo.enode' attach ipc:/home/ubuntu/FastBFT_ethereum/experiment/data1/node${y}/geth.ipc >> enode.txt"
         ((x++));
         ((y++));
 done
 done
+~
