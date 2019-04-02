f86f0980b7732150bae392136e4d57c54e33a491
jianwei20 <102753053@nccu.edu.tw>
2019-04-03 00:16:51 +0800

    node -v /

---
+++ experiment/node5.sh
@@ -0,0 +1,1 @@
+../build/bin/geth --networkid 2234 --port 30307 --rpcport 8549 --datadir "data1/node5" --nodiscover --rpc --rpccorsdomain "*" --rpcapi "eth,net,web3,debug" --bft --allow-empty --num-validators 6 --node-num 4 
