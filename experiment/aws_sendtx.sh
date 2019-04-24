2549378ecc9d0f81546b3738203fc8e38f8a4cbd
Ubuntu <ubuntu@ip-10-0-1-17.us-west-2.compute.internal>
2019-04-24 09:06:53 +0000

    add aws_sendtx.sh

---
+++ experiment/aws_sendtx.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# $1 total number of the node
+# $2 size of block
+# $3 number of the node in the ip
+
+/usr/local/go/bin/go version
+echo "---------writejson1.py--------------"
+
+python writejson.py
+sleep 1.0
+echo "-------go build ethclient-----------"
+
+cd  /home/ubuntu/FastBFT_ethereum/experiment/ethclient
+
+/usr/local/go/bin/go build
+
+cd ..
+sleep 3.0
+
+echo "-----------send.tx---------------"
+
+./sendtx.sh local $3
+
+
+sleep 10.0
+
+cat sendtx.log
+
