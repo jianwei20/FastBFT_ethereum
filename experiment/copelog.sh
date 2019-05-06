bd4699a05df4dc9c956ac65fe19d1cb15cd8fdb2
jianwei20 <102753053@nccu.edu.tw>
2019-05-07 01:01:02 +0800

    sh error

--- experiment/copelog.sh
+++ experiment/copelog.sh
@@ -2,6 +2,9 @@
 # $1 total number of the node
 # $2 size of block
 # $3 number of the node in the ip
+rm -rf result1
+mkdir result1/
+
 array=(10.0.1.70 10.0.1.65)
 user=”ubuntu″
 echo "------------pull Address from enode--------------"
@@ -15,4 +18,4 @@ do
         scp -i ssh-fsbft.pem ubuntu@$i:/home/ubuntu/FastBFT_ethereum/experiment/data1/n$t.log /home/ubuntu/FastBFT_ethereum/experiment/result1
         ((t++))
 done
-donels
+done
