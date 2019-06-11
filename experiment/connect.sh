8e6099ed7b5ef8107f4ca47610ba33e4667f5065
JianWei <102703053@nccu.edu.tw>
2019-06-11 17:54:00 +0800

    Update connect.sh

--- experiment/connect.sh
+++ experiment/connect.sh
@@ -1,2 +1,3 @@
 ../build/bin/geth --exec 'admin.peers.length ' attach ipc:./data1/node1/geth.ipc
-
+../build/bin/geth --exec 'net.listening' attach ipc:./data1/node1/geth.ipc
+../build/bin/geth --exec 'admin.nodeInfo' attach ipc:./data1/node1/geth.ipc
