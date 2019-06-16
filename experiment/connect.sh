f21400c4e8c948a5757db81ad6b7384b122b0422
Ubuntu <ubuntu@ip-10-0-1-38.us-west-2.compute.internal>
2019-06-16 15:52:18 +0000

    d

--- experiment/connect.sh
+++ experiment/connect.sh
@@ -2,7 +2,8 @@
 for ((i<0;i<${1};i++));
 do 
 
-echo node ${1}
+echo node $i
 ../build/bin/geth --exec 'admin.peers.length ' attach ipc:./data1/node$i/geth.ipc
 ../build/bin/geth --exec 'net.peerCount' attach ipc:./data1/node$i/geth.ipc
+
 done
